477ccc7ff66b1ce5ca80fdad373be4f4db96b199
Sébastien Lengagne <lengagne@gmail.com>
2026-01-22 21:41:35 +0100

    modif transformation

--- braccio_tp/braccio_tp/student_work/transformation.py
+++ braccio_tp/braccio_tp/student_work/transformation.py
@@ -10,63 +10,74 @@ class Transformation:
         self.rotation = rotation  # Matrice 3x3
         self.position = position  # Vecteur 3x1
 
-    @classmethod
-    def from_translation(cls, tx, ty, tz):
-        """Crée une transformation à partir d'une translation."""
-        return cls(position=np.array([tx, ty, tz]))
 
-    @classmethod
-    def from_rotation_x(cls, theta):
-        """Crée une transformation à partir d'une rotation autour de l'axe X."""
+    def set_translation(self, tx, ty, tz):
+        self.position[0] = tx
+        self.position[1] = ty
+        self.position[2] = tz
+
+    def set_rotation(self, roll, pitch, yaw):
+        self.rotation = RotationX(roll) # * ...
+
+    def set_rotation_from_quaternion(self,x,y,z,w):
+        self.rotation[0,0] = 1.0
+        self.rotation[0,1] = 0.0
+        self.rotation[0,2] = 0.0
+        self.rotation[1,0] = 0.0
+        self.rotation[1,1] = 1.0
+        self.rotation[1,2] = 0.0
+        self.rotation[2,0] = 0.0
+        self.rotation[2,1] = 0.0
+        self.rotation[2,2] = 1.0
+
+
+    def RotationX(theta):
+        """Crée la matrice de rotation  pour une rotation autour de l'axe X."""
         rotation = np.array([
             [1, 0, 0],
             [0, cos(theta), -sin(theta)],
             [0, sin(theta), cos(theta)]
         ])
-        return cls(rotation=rotation)
+        return rotation
 
-    @classmethod
-    def from_rotation_y(cls, theta):
-        """Crée une transformation à partir d'une rotation autour de l'axe Y."""
+    def RotationY(theta):
+        """Crée la matrice de rotation  pour une rotation autour de l'axe Y."""
         rotation = np.array([
             [cos(theta), 0, sin(theta)],
             [0, 1, 0],
             [-sin(theta), 0, cos(theta)]
         ])
-        return cls(rotation=rotation)
+        return rotation
 
-    @classmethod
-    def from_rotation_z(cls, theta):
-        """Crée une transformation à partir d'une rotation autour de l'axe Z."""
+    def RotationZ(theta):
+        """Crée la matrice de rotation  pour une rotation autour de l'axe Z."""
         rotation = np.array([
             [cos(theta), -sin(theta), 0],
             [sin(theta), cos(theta), 0],
             [0, 0, 1]
         ])
-        return cls(rotation=rotation)
+        return rotation
 
-    def to_homogeneous_matrix(self):
-        """Retourne la matrice de transformation homogène (4x4) associée."""
-        T = np.eye(4)
-        T[0:3, 0:3] = self.rotation
-        T[0:3, 3] = self.position
-        return T
-
-    def compose(self, other):
+    def multiplication(self, other):
         """Compose cette transformation avec une autre (self * other)."""
+        """Attention le signe * sur les np.array fait une multiplication terme à terme """
+        """la multiplication matricielle se fait avec l'opérateur @ """
+
         new_rotation = np.dot(self.rotation, other.rotation)
         new_position = self.position + np.dot(self.rotation, other.position)
+
         return Transformation(new_rotation, new_position)
 
     def inverse(self):
         """Retourne l'inverse de cette transformation."""
         inv_rotation = self.rotation.T
         inv_position = -np.dot(inv_rotation, self.position)
+
         return Transformation(inv_rotation, inv_position)
 
     def __mul__(self, other):
         """Définit l'opérateur * pour composer deux transformations (self * other)."""
-        return self.compose(other)
+        return self.multiplication(other)
 
     def __str__(self):
         """Affiche la transformation sous forme lisible."""
