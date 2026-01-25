21b2d4f918b1110b73e8f462b60bbfb1b800b4f5
Sébastien Lengagne <lengagne@gmail.com>
2026-01-25 22:32:27 +0100

    update with localisation

--- braccio_tp/launch/RunLocalisation.launch.py
+++ braccio_tp/launch/RunLocalisation.launch.py
@@ -11,12 +11,21 @@ def generate_launch_description():
     # Arguments
     pkg_share = get_package_share_directory('braccio_tp')
     pkg_braccion_common = get_package_share_directory('braccio_common')
+    pkg_braccion_robot = get_package_share_directory('braccio_robot')
 
     camera_with_tags_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_braccion_common, 'launch', 'RunCameraWithTags.launch.py')
         ),
     )
+
+
+    rviz2_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_braccion_robot, 'launch', 'SeeRobotOnRviz.launch.py')
+        ),
+    )
+
 
 
     # Chemin vers le fichier de markers
@@ -38,24 +47,10 @@ def generate_launch_description():
         }],
     )
 
-    # lancement rviz
-    rviz_config_file = os.path.join(
-        get_package_share_directory('braccio_tp'),
-        'config',
-        'config_localisation.rviz'
-    )
-
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        arguments=['-d', rviz_config_file],
-        output='screen'
-    )
-
     return LaunchDescription([
         static_markers_arg,
         camera_with_tags_launch,
         localisation_node,
-        rviz_node,
+        rviz2_launch
+
     ])
